102f46d7dd82928618403057df2ef764329e9a7f
pritiInnogent <priti.lalwani@innogent.in>
2025-11-02 23:14:41 +0530

    Update fact_recursion.java

--- fact_recursion.java
+++ fact_recursion.java
@@ -1,38 +1,48 @@
- class FactorialRecursion {
 
-    // Recursive method to calculate factorial
-    public static int factorial(int n) {
-        /*
-         * When factorial(n) is called, a new Stack Frame is created in the CALL STACK.
-         * Inside that stack frame:
-         *   - 'n' (local variable) is stored
-         *   - Return address (where to go back after recursion) is stored
-         * No new objects are created in Heap, only primitive int values are used here.
-         */
 
-        if (n == 0 || n == 1) {
-            // Base condition
-            // This stack frame will return 1 and then get removed (popped) from the stack.
-            return 1;
-        }
 
-        // Recursive call
-        return n * factorial(n - 1);
-        /*
-         * At this point:
-         *   - factorial(n) is paused, waiting for factorial(n-1).
-         *   - factorial(n-1) is pushed into the call stack.
-         * This continues until base case is reached.
-         */
+import java.util.Scanner;
+import java.math.BigInteger;
+
+class Factorial {
+
+ 
+    static BigInteger factorial(int n) {
+       
+        if (n == 0 || n == 1)
+            return BigInteger.ONE;
+        else
+            return BigInteger.valueOf(n).multiply(factorial(n - 1));
     }
 
     public static void main(String[] args) {
-        /*
-         * 'num' and 'result' are local variables stored in the main() method's stack frame.
-         * main() method itself has a stack frame in the Call Stack.
-         */
-        int num = 5;  
-        int result = factorial(num);  // function call → new stack frame created
-        System.out.println("Factorial of " + num + " is: " + result);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number to find factorial: ");
+        int num = sc.nextInt();
+
+        // ---------- MEMORY MANAGEMENT & LIMIT CHECK ----------
+        // Each recursive call consumes stack memory for:
+        //  - Function call frame
+        //  - Local variables
+        //  - Return address
+        //
+        /
+
+        if (num < 0) {
+            System.out.println(" Factorial not defined for negative numbers.");
+        } 
+        else if (num > 20000) {
+            System.out.println(" Number too large! Recursive factorial may cause StackOverflowError or memory exhaustion.");
+            System.out.println("Please enter a number less than or equal to 20000.");
+        } 
+        else {
+            BigInteger result = factorial(num);
+            System.out.println("\n Factorial of " + num + " is:\n" + result);
+        }
+
+        sc.close();
     }
 }
+
+
